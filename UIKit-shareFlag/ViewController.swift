11a0b7a0318bace3a0979f2a372be666ca53d50d
Akshay Mahajan <maha0134@algonquinlive.com>
2023-05-24 18:07:04 -0400

    initialize tableviewcell and add country names

--- UIKit-shareFlag/ViewController.swift
+++ UIKit-shareFlag/ViewController.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+	var images = [String]()
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		let fm = FileManager.default
 		//The app will always have this path
 		let path = Bundle.main.resourcePath!
 		let contents = try! fm.contentsOfDirectory(atPath: path)
-		var images = [String]()
+		
 		for image in contents {
-			if image.hasSuffix(".png") {
+			if image.hasSuffix("2x.png") {
 				images.append(image)
 			}
 		}
-		print(images)
+		title = "Flags of Countries"
 	}
-
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return images.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+		cell.textLabel?.text = String(images[indexPath.row].split(separator: "@")[0]).uppercased()
+		return cell
+	}
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+	}
 
 }
 
